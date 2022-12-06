002962b9e3ca1fbb839919d67ddc7ab1b5d7a5bb
hoheesu <hoheesu@naver.com>
2022-12-06 20:01:24 +0900

    feat: Create: index.html <header> & swiper.js

---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+let swiper = new Swiper(".mySwiper1", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: 'ture',
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+export {swiper}
