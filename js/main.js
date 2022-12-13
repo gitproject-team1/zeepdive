f3b345724c24e74d4183f93c3ce41caced58c569
hoheesu <hoheesu@naver.com>
2022-12-13 16:30:39 +0900

    fix: merge error

--- js/main.js
+++ js/main.js
@@ -5,16 +5,42 @@ import {
   createSubmitEvent,
   createLoginEvent,
   getItemWithExpireTime,
+  loginModal,
+  validationStyle,
+  pwchange,
 } from "./signup.js";
+import { authLogin, editUser } from "./requests.js";
+import { renderAdminItems } from "./admin.js";
+import { getItem } from "./requests.js";
+import { renderMainItems, renderCategoryPages } from "./render.js";
+
+import { searchForm, searchInput } from "./store.js";
+
 import {
-  authLogin,
-  editUser,
-  removeAccount,
-} from "./requests.js";
-import {
-  createItemEvent,
-  renderAdminItems
-} from "./admin.js";
+  submitEl,
+  emailInputEl,
+  passwordcheckEl,
+  passwordInputEl,
+  loginBtn,
+  loginId,
+  loginBtnEl,
+  backGround,
+  idboxEl,
+  emailErrorMsg,
+  singupEmailBox,
+  pwErrorMsg,
+  signupPwBox,
+  signupRepwBox,
+  pwLengthMsg,
+  idErrorMsg,
+  exptext,
+  userInfoName,
+  nameChangeBtn,
+  pwChangeBtn,
+  userModal,
+  userModalBtn,
+  userModalContent,
+} from "./store.js";
 
 import {
   accountAddSubmit,
@@ -26,54 +52,19 @@ import {
 const ADMIN_EMAIL = `hyochofriend@naver.com`;
 
 const firstNav = document.querySelector("ul.nav-1depth > li:first-child");
-const backGround = document.querySelector(".back-ground");
-export const loginBtnEl = document.querySelector(".login");
-const loginModal = document.querySelector(".login-modal");
-const signupModal = document.querySelector(".signup-modal");
-const mainPgEl = document.querySelector(".main-page");
+export const mainPgEl = document.querySelector(".main-page");
 const userPgEl = document.querySelector(".user-page");
 const adminPgEl = document.querySelector(".admin-page");
+const footerEl = document.querySelector("footer");
+const categorypgEl = document.querySelector(".category-page");
 
-// signup elements
-export const emailInputEl = document.getElementById("signup-email");
-export const passwordInputEl = document.getElementById("signup-pw");
-export const passwordcheckEl = document.getElementById("signup-repw");
-export const displayNameInputEl = document.getElementById("signup-name");
-export const submitEl = document.getElementById("frm");
+// 검색창
+searchForm.addEventListener("submit", (event) => {
+  window.location.href = `#/furniture/all/${searchInput.value}`;
+});
 
-// login elements
-export const loginId = document.querySelector(".login-id");
-export const loginPw = document.querySelector(".login-pw");
-export const loginBtn = document.querySelector(".login-btn");
-export const idboxEl = document.querySelector(".id-box");
-export const pwboxEl = document.querySelector(".pw-box");
-export const loginErrorBox = document.querySelector(".login-error-box");
-
-//search elements
-const searchInput = document.getElementById("search-main");
-
-// admin elements
-const addItemBtn = document.querySelector(".submit-item");
-const adminThumbnailFile = document.getElementById("admin-info-thumbnail");
-const adminImgFile = document.getElementById("admin-info-img");
-export const adminItemsEl = document.querySelector(".item-container");
-
-// user Info elements
-const pwChangeBtn = document.querySelector(".pw-change-btn");
-export const userInfoName = document.getElementById("user-info-name");
-export const nameChangeBtn = document.querySelector(".name-change-btn");
-export const userInfoPw = document.getElementById("user-info-pwd");
-export const userInfoNewPw = document.getElementById("user-info-new-pwd");
-
-const removeAccountBtn = document.querySelector('.remove-account')
-export const bankSubmitBtn = document.querySelector('.bank-add-btn');
-export const bankSelectEl = document.querySelector('.bank-select');
-export const listUl = document.querySelector('.account-lists')
-export const removeSectionBtn = document.querySelector('.bank-remove-gnb')
-export const addSectionBtn = document.querySelector('.bank-add-gnb')
-
-
-
+//상세페이지
+const detailPageEl = document.querySelector(".detail-container");
 
 firstNav.addEventListener("mouseover", () => {
   backGround.style.visibility = "visible";
@@ -82,53 +73,65 @@ firstNav.addEventListener("mouseout", () => {
   backGround.style.visibility = "hidden";
 });
 
+// ============ 인증 관련 ============
 // 로그인/회원가입 모달 visibility 조정
-loginBtnEl.addEventListener("click", () => {
-  if (loginBtnEl.textContent === "로그인/가입") {
-    backGround.style.visibility = "visible";
-    loginModal.style.visibility = "visible";
-    document.querySelector(".close-login").addEventListener("click", () => {
-      backGround.style.visibility = "hidden";
-      loginModal.style.visibility = "hidden";
-    });
-    document.querySelector(".signup").addEventListener("click", () => {
-      signupModal.style.visibility = "visible";
-      loginModal.style.visibility = "hidden";
-      document.querySelector(".close-signup").addEventListener("click", () => {
-        backGround.style.visibility = "hidden";
-        signupModal.style.visibility = "hidden";
-      });
-    });
+loginBtnEl.addEventListener("click", loginModal);
+// 회원가입 전송
+submitEl.addEventListener("submit", createSubmitEvent);
+// 로그인
+loginBtn.addEventListener("click", createLoginEvent);
+
+// 로그인 시 유효성 검사
+loginId.addEventListener("focusout", () => {
+  if (loginId.value && !exptext.test(loginId.value)) {
+    validationStyle(idErrorMsg, "add", idboxEl, "#ed234b");
   }
 });
-// 평소에는 display: none을 걸어놓는다.
-// 이름 변경을 클릭하고 완료 했으면 모달창이 뜨도록
-// 확인 버튼을 누르면 다시 display: none 되도록
-export const userModal = document.querySelector(".user-modal");
-const userModalBtn = document.querySelector(".user-modal-btn");
-export const userModalContent = document.querySelector(".user-modal-content");
-submitEl.addEventListener("submit", createSubmitEvent);
-loginBtn.addEventListener("click", createLoginEvent);
-export const content = "";
+loginId.addEventListener("focusin", () => {
+  validationStyle(idErrorMsg, "remove", idboxEl, "#999");
+});
+
+// 회원가입 유효성 검사
+// 이메일
+emailInputEl.addEventListener("focusout", () => {
+  if (emailInputEl.value && !exptext.test(emailInputEl.value)) {
+    validationStyle(emailErrorMsg, "add", singupEmailBox, "#ed234b");
+  }
+});
+emailInputEl.addEventListener("focusin", () => {
+  validationStyle(emailErrorMsg, "remove", singupEmailBox, "#333");
+});
+// 비밀번호 8자리 이상
+passwordInputEl.addEventListener("focusout", () => {
+  if (passwordInputEl.value && passwordInputEl.value.length < 8) {
+    validationStyle(pwLengthMsg, "add", signupPwBox, "#ed234b");
+  }
+});
+passwordInputEl.addEventListener("focusin", () => {
+  validationStyle(pwLengthMsg, "remove", signupPwBox, "#333");
+});
+// 비밀번호 확인
+passwordcheckEl.addEventListener("focusout", () => {
+  if (passwordInputEl && passwordInputEl.value !== passwordcheckEl.value) {
+    validationStyle(pwErrorMsg, "add", signupRepwBox, "#ed234b");
+  }
+});
+passwordcheckEl.addEventListener("focusin", () => {
+  validationStyle(pwErrorMsg, "remove", signupRepwBox, "#333");
+});
+
+// ============ 인증 관련 : 회원정보 페이지 ============
 // 이름 옆에 변경 버튼 누르면 이름 변경되도록 만들기
 nameChangeBtn.addEventListener("click", async (event) => {
   event.preventDefault();
-  await editUser("이름", userInfoName.value);
+  if (userInfoName.value) await editUser("이름", userInfoName.value);
 });
 // 변경 됐다는 모달창에 있는 확인 버튼
 userModalBtn.addEventListener("click", () => {
   userModal.classList.remove("show");
 });
 // 비밀번호 변경 버튼 누르면 비밀번호 변경되도록 만들기
-pwChangeBtn.addEventListener("click", async (event) => {
-  event.preventDefault();
-  await editUser(
-    "비밀번호",
-    userInfoName.value,
-    userInfoPw.value,
-    userInfoNewPw.value
-  );
-});
+pwChangeBtn.addEventListener("click", pwchange);
 
 // 로컬에 로그인 데이터 있는지 확인.
 (async () => {
@@ -143,44 +146,6 @@ pwChangeBtn.addEventListener("click", async (event) => {
   getItemWithExpireTime("token");
 })();
 
-// ============ 관리자페이지 ============
-let base64Thumbnail = "";
-let base64Img = "";
-
-adminThumbnailFile.addEventListener("change", (event) => {
-  const {
-    files
-  } = event.target;
-  for (let i = 0; i < files.length; i += 1) {
-    const file = files[i];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.addEventListener("load", (e) => {
-      base64Thumbnail = e.target.result;
-    });
-  }
-});
-
-adminImgFile.addEventListener("change", (event) => {
-  const {
-    files
-  } = event.target;
-  for (let i = 0; i < files.length; i += 1) {
-    const file = files[i];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.addEventListener("load", (e) => {
-      base64Img = e.target.result;
-    });
-  }
-});
-
-addItemBtn.addEventListener("click", async (event) => {
-  event.preventDefault();
-  await createItemEvent(base64Thumbnail, base64Img);
-  location.reload();
-});
-
 // 초기화면(새로고침, 화면진입) 렌더
 router();
 
@@ -192,25 +157,58 @@ async function router() {
   const routePath = location.hash;
   // 초기화면
   if (routePath === "") {
-    mainPgEl.style.display = "block";
+    detailPageEl.style.display = "block";
+    mainPgEl.style.display = "none";
     userPgEl.style.display = "none";
     adminPgEl.style.display = "none";
+    footerEl.style.display = "none";
+    categorypgEl.style.display = "none";
+    await renderMainItems();
+    mainPgEl.style.display = "block";
+    footerEl.style.display = "block";
+    //회원정보 페이지
   } else if (routePath.includes("#/user")) {
-    // 기존꺼 hide하고 갈기면됨
     mainPgEl.style.display = "none";
     userPgEl.style.display = "block";
     adminPgEl.style.display = "none";
+    detailPageEl.style.display = "none";
+    categorypgEl.style.display = "none";
+    //제품 상세정보 페이지
+  } else if (routePath.includes("#/detail")) {
+    mainPgEl.style.display = "none";
+    userPgEl.style.display = "none";
+    adminPgEl.style.display = "none";
+    detailPageEl.style.display = "block";
+    categorypgEl.style.display = "none";
+    //관리자 페이지
   } else if (routePath.includes("#/admin")) {
+    // 관리자인지 확인
     const email = await authLogin();
-    console.log(email);
+    detailPageEl.style.display = "none";
+    categorypgEl.style.display = "none";
+    //만약 관리자라면,
     if (email === ADMIN_EMAIL) {
       mainPgEl.style.display = "none";
       userPgEl.style.display = "none";
       adminPgEl.style.display = "block";
       renderAdminItems();
     } else {
+      // 허가되지 않은 사용자면 -> alert띄운다
       alert("허용되지 않은 접근입니다.");
     }
+    //category 분류 페이지
+  } else if (routePath.includes("#/furniture")) {
+    detailPageEl.style.display = "none";
+    mainPgEl.style.display = "none";
+    userPgEl.style.display = "none";
+    adminPgEl.style.display = "none";
+    categorypgEl.style.display = "block";
+    // category url에서 파싱
+    console.log(routePath);
+    const category = routePath.split("/")[2];
+    let searchKeyword = routePath.split("/")[3];
+    searchKeyword = decodeURIComponent(searchKeyword);
+    await renderCategoryPages(category, searchKeyword);
   }
 }
 
@@ -220,51 +218,89 @@ userInfoBtn.addEventListener("click", () => {
   const token = localStorage.getItem("token");
   if (token) {
     window.location = "#/user";
-  } else return;
+  } else {
+    userModalContent.innerHTML = `로그인을 해주세요.`;
+    userModal.classList.add("show");
+  }
 });
 
-// user-info창에서 은행을 선택하면 생기는 이벤트
-bankSelectEl.addEventListener('change', (event) => {
-  bankSelelectEvent(event)
-})
+// bank elements
+const inputBankEl1 = document.querySelector(".bank-link-1");
+const inputBankEl2 = document.querySelector(".bank-link-2");
+const inputBankEl3 = document.querySelector(".bank-link-3");
+const inputBankEl4 = document.querySelector(".bank-link-4");
+const allInputBankEl = document.querySelectorAll(".bank-link-input");
+const bankSelectEl = document.querySelector(".bank-select");
+const bankSubmitBtn = document.querySelector(".bank-link-btn");
+let bankNumber = "";
 
-bankSubmitBtn.addEventListener('click', (event) => {
-  accountAddSubmit(event)
-  window.alert('계좌가 추가 되었습니다.')
-})
-
-
-removeSectionBtn.addEventListener('click', (event) => {
-  event.stopPropagation()
-  listUl.innerHTML = ''
-  renderUserAccount()
-
-  const clickValue = removeSectionBtn.classList.contains('on')
-  gnbBtnClick('remove', clickValue)
-})
-
-addSectionBtn.addEventListener('click', () => {
-  const clickValue = addSectionBtn.classList.contains('on')
-  gnbBtnClick('add', clickValue)
-})
-
-removeAccountBtn.addEventListener('click', async()=>{
-  const accountCheckEl = document.querySelectorAll('.account-lists>li>#remove-check')
-  const accountList = document.querySelectorAll('.account-lists>li')
-  let arr = []
-  accountCheckEl.forEach( (e,i) => {
-    if(e.checked){
-      arr.push(document.getElementById(accountList[i].getAttribute('id')).getAttribute('id'))
-    }
-  })
-  const accountRemoveSign = document.querySelector('#account-remove-sign')
-  for(const x of arr){
-    await removeAccount(x,accountRemoveSign.checked)
+bankSelectEl.addEventListener("change", () => {
+  let select1 = bankSelectEl[bankSelectEl.selectedIndex].value;
+  let arr = [];
+  if (select1 === "none") {
+    inputDisplay("none");
+  } else if (select1 === "bank-nh") {
+    inputDisplay("inline");
+    arr = [3, 4, 4, 2];
+  } else if (select1 === "bank-kb") {
+    inputDisplay("inline");
+    arr = [3, 2, 4, 3];
+  } else if (select1 === "bank-sh") {
+    inputDisplay("inline");
+    arr = [3, 3, 6];
+  } else if (select1 === "bank-ka") {
+    inputDisplay("inline");
+    arr = [4, 2, 7];
   }
-  window.alert('삭제되었습니다.')
-  listUl.innerHTML = ''
-  accountRemoveSign.checked = false
-  getAccountsInfo()
-  
-})
 
+  inputBankEl1.value = "";
+  inputBankEl2.value = "";
+  inputBankEl3.value = "";
+  inputBankEl4.value = "";
+
+  inputBankEl1.setAttribute("maxlength", arr[0]);
+  inputBankEl2.setAttribute("maxlength", arr[1]);
+  inputBankEl3.setAttribute("maxlength", arr[2]);
+  inputBankEl4.setAttribute("maxlength", arr[3]);
+
+  const i4Len = inputBankEl4.getAttribute("maxlength");
+
+  if (i4Len === "undefined") {
+    inputBankEl4.style.display = "none";
+  } else {
+    inputBankEl4.style.display = "inline";
+  }
+  inputBankEl1.focus();
+
+  function inputDisplay(display) {
+    allInputBankEl.forEach((e) => {
+      e.style.display = display;
+    });
+  }
+});
+
+allInputBankEl.forEach((e, i) => {
+  e.addEventListener("input", () => {
+    if (i < 3) {
+      if (
+        allInputBankEl[i].value.length ===
+        Number(allInputBankEl[i].getAttribute("maxlength"))
+      ) {
+        allInputBankEl[i + 1].focus();
+      }
+    }
+  });
+});
+bankSubmitBtn.addEventListener("click", () => {
+  allInputBankEl.forEach((e) => {
+    bankNumber += e.value;
+    e.value = "";
+  });
+  if (!Number(bankNumber)) {
+    window.alert("숫자만 입력하세요");
+  } else {
+    // console.log(Number(bankNumber));
+  }
+  // console.log(bankNumber);
+  bankNumber = "";
+});
