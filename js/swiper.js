5777ed49a5458916d6a1c442d67ddf8e92528776
JYROH <ghgt1@naver.com>
2022-12-16 01:28:32 +0900

    fix: Fix account title not displaying

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,6 @@
 import { getAccounts } from "./requests.js";
 import { availableIndex } from "./render.js";
 
-const bankMatch = {
-  0: "케이뱅크",
-  1: "하나은행",
-  2: "카카오뱅크",
-  3: "NH농협은행",
-  4: "신한은행",
-  5: "우리은행",
-  6: "KB국민은행",
-};
-
 export const swiper = new Swiper(".mySwiper1", {
   autoplay: {
     delay: 5000,
