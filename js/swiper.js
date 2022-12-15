6d453e71e6ad944bc78887b19dcfe5398dfec346
hyorimm <hyorimm@gmail.com>
2022-12-15 15:46:54 +0900

    feat: Create account swiper

--- js/swiper.js
+++ js/swiper.js
@@ -3,7 +3,7 @@ export const swiper = new Swiper(".mySwiper1", {
     delay: 5000,
     disableOnInteraction: false,
   },
-  loop: "ture",
+  loop: "true",
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -12,4 +12,17 @@ export const swiper = new Swiper(".mySwiper1", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+export const accountSwiper = new Swiper(".account-swiper", {
+  loop: "true",
+  pagination: {
+    el: ".account-swiper .swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".account-swiper .swiper-button-next",
+    prevEl: ".account-swiper .swiper-button-prev",
+  },
+  slidesPerView: 3,
 });
