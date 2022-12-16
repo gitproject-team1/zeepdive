9a9b6555f6f178054e3409eb929e89480fcace9f
1myeji <106291546+1myeji@users.noreply.github.com>
2022-12-16 14:06:42 +0900

    fix: cartpage rendering problem
    장바구니에 아무것도 없을 경우 나오는 style을
    장바구니에 delete 버튼을 누를 때만 동작하도록 했는데 고쳤습니다.

--- js/render.js
+++ js/render.js
@@ -581,7 +581,12 @@ export async function renderPurchasePages(items) {
 let itemsPrice = 0;
 export async function renderCartPages() {
   const email = await authLogin();
-  const cartIdArr = JSON.parse(localStorage.getItem(`cartId-${email}`));
+  const cartIdArr = JSON.parse(localStorage.getItem(`cartId-${email}`)) || [];
+  console.log(cartIdArr);
+  if (cartIdArr.length === 0) {
+    emptyCart();
+    return;
+  }
   const promises = [];
   for (const id of cartIdArr) {
     promises.push(getDetailItem(id));
@@ -616,7 +621,6 @@ export async function renderCartPages() {
     const cartDelete = element.querySelector(".cart-delete");
     cartDelete.addEventListener("click", (event) => {
       deleteCartItems(event);
-      renderPrice();
     });
   }
   renderPrice();
@@ -645,13 +649,19 @@ async function deleteCartItems(event) {
     return incartItem.dataset.id !== cartIdEl;
   });
   if (arr.length === 0) {
+    emptyCart();
     localStorage.removeItem(`cartId-${email}`);
-    cartItems.innerHTML = /* html */ `
-      <p class="cart-empty">장바구니에 담긴 상품이 없습니다</p>
-    `;
-    deliveryPrice.textContent = "0원";
-    totalPrice.textContent = "0원";
     return;
   }
   localStorage.setItem(`cartId-${email}`, JSON.stringify(arr));
+  renderPrice();
+}
+
+function emptyCart() {
+  cartItems.innerHTML = /* html */ `
+  <p class="cart-empty">장바구니에 담긴 상품이 없습니다</p>
+  `;
+  singlePrice.textContent = "0원";
+  deliveryPrice.textContent = "0원";
+  totalPrice.textContent = "0원";
 }
